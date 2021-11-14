a6a92826a8d333c701f712bc3a6785ab18821da9
EdisonXie <68284121+EdisonXie@users.noreply.github.com>
2021-11-14 18:55:14 -0500

    Do-Re-Mi Skeleton

---
+++ Do-Re-Mi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Do-Re-Mi
+//
+//  Created by Edison Xie on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
