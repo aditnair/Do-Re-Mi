c45284c7f25e270a8464b78acb47a2dac5241af8
gabrielbrosula <82014893+gabrielbrosula@users.noreply.github.com>
2021-11-14 20:16:38 -0500

    Setup Spotify iOS SDK

---
+++ Do-Re-Mi/DoReMe-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  DoReMe-Bridging-Header.h
+//  Do-Re-Mi
+//
+//  Created by Gabriel Brosula on 11/14/21.
+//
+
+#ifndef DoReMe_Bridging_Header_h
+#define DoReMe_Bridging_Header_h
+
+#import <Spotify/SpotifyiOS.h>
+
+
+#endif /* DoReMe_Bridging_Header_h */
